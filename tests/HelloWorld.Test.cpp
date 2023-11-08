9233072c31c21cbe156274a4cd419d0707b1c78a
KikoMira <145770964+KikoMira@users.noreply.github.com>
2023-11-08 20:14:17 +0100

    variable test added

--- tests/HelloWorld.Test.cpp
+++ tests/HelloWorld.Test.cpp
@@ -4,5 +4,13 @@ using namespace Easy_Cpp;
 
 int main()
 {
+    //Print function test
     print("Hello World");
+
+    //variable test
+    auto test1 = var(1)
+    auto test2 = tvar("Hello World")
+
+    print(test1);
+    print(test2);
 }
